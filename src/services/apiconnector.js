3f832e30862748e0f0c68a113b6adcb64c205b98
sourabh <sourabhtanwar18@gmail.com>
2025-07-30 11:04:44 +0530

    Updating apiconnector.js

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -1,6 +1,13 @@
 import axios from "axios"
 
-export const axiosInstance = axios.create({});
+const axiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_BASE_BACKEND_URL,
+    withCredentials: true,
+    timeout: 5000,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
     return axiosInstance({
