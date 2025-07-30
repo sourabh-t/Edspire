ca9e082e0a9bb200aebfa1fce68ec1a11ef8ce2b
sourabh <sourabhtanwar18@gmail.com>
2025-07-30 09:49:58 +0530

    Updating index.ja

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL || "http://localhost:3000",
+        origin: process.env.FRONTEND_URL,
         credentials: true,
     })
 );
