069cfcbc2fd8ab294226b2e1fe672df6b5062795
sourabh <sourabhtanwar18@gmail.com>
2025-07-30 10:57:03 +0530

    Updating index.js

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(
     cors({
         origin: process.env.FRONTEND_URL ? 
             [process.env.FRONTEND_URL, "http://localhost:3000"] : 
-            ["http://localhost:3000", "https://edspire-frontend.vercel.app/"],
+            ["http://localhost:3000", "https://edspire-bocj.vercel.app/"],
         credentials:true,
     })
 );
